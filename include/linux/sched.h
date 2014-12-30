57fe78c29b90b77b85c3dc1deae45a1d5c487e82
Yi-wei Zhao <gbjc64@motorola.com>
2014-12-30 16:48:30 -0600

    drivers:lmk: implement task's adj rbtree
    Based on the current LMK implementation, LMK has to
    scan all processes to select the correct task to kill
    during low memory.
    
    The basic idea for the optimization is to : queue all
    tasks with oom_score_adj priority, and then LMK just
    selects the proper task from the queue(rbtree) to kill.
    
    performance improvement:
    current: average time to find a task to kill : 1004us
    optimized: average time to find a task to kill: 43us
    
    Change-Id: I32504e9f2f370d58c038eea7457d95c8ed8b6b9b
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/701205
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1413,6 +1413,9 @@ struct task_struct {
 #endif
 
 	struct list_head tasks;
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	struct rb_node adj_node;
+#endif
 #ifdef CONFIG_SMP
 	struct plist_node pushable_tasks;
 	struct rb_node pushable_dl_tasks;
@@ -1837,6 +1840,30 @@ static inline struct pid *task_tgid(struct task_struct *task)
 {
 	return task->group_leader->pids[PIDTYPE_PID].pid;
 }
+
+
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+extern void add_2_adj_tree(struct task_struct *task);
+extern void delete_from_adj_tree(struct task_struct *task);
+#else
+static inline void add_2_adj_tree(struct task_struct *task) { }
+static inline void delete_from_adj_tree(struct task_struct *task) { }
+#endif
+
+#ifdef CONFIG_TASK_CPUFREQ_STATS
+static inline void task_update_time_in_state(struct task_struct *task, int cpu)
+{
+	update_time_in_state(task, cpu);
+}
+
+static inline void task_update_cumulative_time_in_state(struct task_struct *task,
+						 struct task_struct *parent,
+						 int cpu)
+{
+	update_cumulative_time_in_state(task, parent, cpu);
+}
+#endif
+
 
 /*
  * Without tasklist or rcu lock it is not safe to dereference
