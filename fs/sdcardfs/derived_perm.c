bb20b54ed71f6414dc286421375f180dc967a9fb
Daniel Rosenberg <drosen@google.com>
2017-03-08 17:45:46 -0800

    ANDROID: sdcardfs: Use d_invalidate instead of drop_recurisve
    drop_recursive did not properly remove stale dentries.
    Instead, we use the vfs's d_invalidate, which does the proper cleanup.
    
    Additionally, remove the no longer used drop_recursive, and
    fixup_top_recursive that that are no longer used.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: Ibff61b0c34b725b024a050169047a415bc90f0d8

--- fs/sdcardfs/derived_perm.c
+++ fs/sdcardfs/derived_perm.c
@@ -296,19 +296,6 @@ void fixup_perms_recursive(struct dentry *dentry, struct limit_search *limit) {
 	__fixup_perms_recursive(dentry, limit, 0);
 }
 
-void drop_recursive(struct dentry *parent) {
-	struct dentry *dentry;
-	list_for_each_entry(dentry, &parent->d_subdirs, d_u.d_child) {
-		if (dentry->d_inode) {
-			mutex_lock(&dentry->d_inode->i_mutex);
-			get_derived_permission(parent, dentry);
-			fix_derived_permission(dentry->d_inode);
-			get_derive_permissions_recursive(dentry);
-			mutex_unlock(&dentry->d_inode->i_mutex);
-		}
-	}
-	spin_unlock(&parent->d_lock);
-}
 
 /* main function for updating derived permission */
 inline void update_derived_permission_lock(struct dentry *dentry)
